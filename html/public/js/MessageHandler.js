70b43068352e17a4936ccc1642841f003628706a
TheAschr <>
2018-02-22 19:46:14 -0600

    Migrated most of form submissions to socket.io

--- html/public/js/MessageHandler.js
+++ html/public/js/MessageHandler.js
@@ -19,16 +19,58 @@ function MessageHandler(toastr,socket,callbacks){
 
     socket.on('message',function(message){
         if(message.error){
-            toastr.error(message.error);
-            if(callbacks && callbacks.on_error){
-                callbacks.on_success(message);
+            toastr.options.closeButton = true;
+            toastr.options.timeOut = 0;
+            toastr.options.preventDuplicates = true;
+
+            let error_msg = '';
+            if(typeof(message.error) == 'object'){
+                for(let i in message.error){
+                    if(message.error.hasOwnProperty(i)){
+                        error_msg+='- '+message.error[i]+'<br>';
+                    }
+                }
+                message.error = error_msg;
             }
+
+
+            if(callbacks && callbacks.on_error){
+                callbacks.on_error(message);
+            }else{
+                if(message.title){
+                    toastr.error(message.error,message.title);
+                }else{
+                    toastr.error(message.error);
+                }
+            }
+
+            toastr.options.closeButton = false;
+            toastr.options.timeOut = 5000;
+            toastr.options.preventDuplicates = false;
         }
+
         if(message.success){
-            toastr.success(message.success);
+            let success_msg = '';
+            if(typeof(message.success) == 'object'){
+                for(let i in message.success){
+                    if(message.success.hasOwnProperty(i)){
+                        success_msg+='- '+message.success[i]+'<br>';
+                    }
+                }    
+                message.success = success_msg;
+            }
+
+
             if(callbacks && callbacks.on_success){
                 callbacks.on_success(message);
+            }else{
+                if(message.title){
+                    toastr.success(message.success,message.title);
+                }else{
+                    toastr.success(message.success);
+                }
             }
         }
+
     });  
 }
