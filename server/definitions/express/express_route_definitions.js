c60cde9cd2d25e21916f2c65d86f663f5d982746
als9xd <als9xd@mst.edu>
2018-03-21 19:03:36 -0500

    Page is working now

--- server/definitions/express/express_route_definitions.js
+++ server/definitions/express/express_route_definitions.js
@@ -680,21 +680,21 @@ module.exports = function(express_conn,pg_conn,socket_io_conn) {
 					pg_conn.client.query(
 						"SELECT * FROM tournaments where id = $1",
 						[
-					  	req.query['id'] // This is whatever '4bar.org/tournaments/id=' is set to
+					  		req.query['id'] // This is whatever '4bar.org/tournaments/id=' is set to
 						],
 						function(err,results){
 							if(err){
 								console.log(err);
 								return;
 							}
+							if(typeof results.rows === 'undefined' || results.rows.length ){
+								res.render('public/error',{error:'No tournament with id of '+req.query['id']});
+								return;
+							}
 							res.render(
-								'/tournaments',  //This is handlebars filename
+								'private/tournaments',  //This is handlebars filename
 							 	{
-									tournament_name: results.rows[0].name,
-									tournament_loc: results.rows[0].location,
-									tournament_limit: results.rows[0].attendee_limit,
-									tournament_deadline: results.rows[0].signup_deadline,
-									tournament_date: results.rows[0].start_date
+							 		tournament_data: results.rows
 								}
 							);
 						}
