9a003c692cb9ba223cc890945713aedce0c64dd6
Alex Schrimpf <als9xd@mst.edu>
2018-04-24 21:40:26 -0500

    Various css fixes,fixes profile tournaments bug, closes #42

--- client/html/public/js/results_tables.js
+++ client/html/public/js/results_tables.js
@@ -8,16 +8,33 @@ $(document).ready(function($) {
       tables[table_titles[i].id] = document.getElementById(table_titles[i].getAttribute('data-toggle'));
     }
 
-    $('html').click(function(evt){    
+    $('html').on('click',function(evt){    
+  
+      if($(evt.target).closest('.search-results-table').length){
+        return;       
+      }  
+
+      for(let i = 0; i < table_titles.length;i++){
+        if($(table_titles[i]).attr('id') !== evt.target.id ){
+            table_titles[i].style.background = '#292b2c';
+            let table = document.getElementById(table_titles[i].getAttribute('data-toggle'));
+            if(table){
+              table_titles[i].parentElement.removeChild(
+                table
+              );
+            }
+            table_titles[i].parentElement.classList.remove('col-sm');
+            table_titles[i].parentElement.classList.add('disabled-col');
+        }else{
+            table_titles[i].style.background = 'rgba(0,0,0,0.7)';
+            table_titles[i].parentElement.appendChild(tables[table_titles[i].id]);
+            table_titles[i].parentElement.classList.remove('disabled-col');
+            table_titles[i].parentElement.classList.add('col-sm');
+            $(tables[table_titles[i].id]).find('.extra').css('display','table-cell');
+        }
+      }
+
       if($(evt.target).closest('#search-results-container').length){
-        return;       
-      }
-
-      if($(evt.target).closest('#sidebar-wrapper').length){
-        return;       
-      }
-
-      if($(evt.target).closest('#navbar-wrapper').length){
         return;       
       }
 
@@ -29,29 +46,9 @@ $(document).ready(function($) {
         $(tables[table_titles[i].id]).find('.extra').css('display','none');
       }
 
-    });                    
-
-    $(".search-results-table-title").click(function() {
-        for(let i = 0; i < table_titles.length;i++){
-          if($(table_titles[i]).attr('id') !== $(this).attr('id')){
-              table_titles[i].style.background = '#292b2c';
-              let table = document.getElementById(table_titles[i].getAttribute('data-toggle'));
-              if(table){
-                table_titles[i].parentElement.removeChild(
-                  table
-                );
-              }
-              table_titles[i].parentElement.classList.remove('col-sm');
-              table_titles[i].parentElement.classList.add('disabled-col');
-          }else{
-              table_titles[i].style.background = 'rgba(0,0,0,0.7)';
-              table_titles[i].parentElement.appendChild(tables[table_titles[i].id]);
-              table_titles[i].parentElement.classList.remove('disabled-col');
-              table_titles[i].parentElement.classList.add('col-sm');
-              $(tables[table_titles[i].id]).find('.extra').css('display','table-cell');
-          }
-        }
     });
+
+
 });
 
 let __getUrlParametergetUrlParameter = function getUrlParameter(sParam) {
