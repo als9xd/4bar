168e0ebbfc268e2bafd5ba12ea6189aa583e8cba
Schrimpf Alexander <als9xd@mst.edu>
2018-04-27 14:36:00 -0500

    Enhanced tables for search results and profiles page

--- client/html/public/js/public.js
+++ client/html/public/js/public.js
@@ -30,3 +30,50 @@ function format_date(unformated,settings){
     return mm+'/'+dd+'/'+yyyy;
   }
 }
+
+function getUrlParameter(sParam) {
+    let sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var updateQueryStringParam = function (key, value) {
+
+    var baseUrl = [location.protocol, '//', location.host, location.pathname].join(''),
+        urlQueryString = document.location.search,
+        newParam = key + '=' + value,
+        params = '?' + newParam;
+
+    // If the "search" string exists, then build params from it
+    if (urlQueryString) {
+
+        updateRegex = new RegExp('([\?&])' + key + '[^&]*');
+        removeRegex = new RegExp('([\?&])' + key + '=[^&;]+[&;]?');
+
+        if( typeof value == 'undefined' || value == null || value == '' ) { // Remove param if value is empty
+
+            params = urlQueryString.replace(removeRegex, "$1");
+            params = params.replace( /[&;]$/, "" );
+
+        } else if (urlQueryString.match(updateRegex) !== null) { // If param exists already, update it
+
+            params = urlQueryString.replace(updateRegex, "$1" + newParam);
+
+        } else { // Otherwise, add it to end of query string
+
+            params = urlQueryString + '&' + newParam;
+
+        }
+
+    }
+    window.history.replaceState({}, "", baseUrl + params);
+};
